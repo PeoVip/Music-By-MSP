3f1c8a60bff4d9ac6f4a18e097b12f7f77f27a2c
PeoVip <levanpeo1213@gmail.com>
2025-02-16 22:39:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "Nghe Nhạc",  // Available activity types : LISTENING , PLAYING
+  activityName: "King Of Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
