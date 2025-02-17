4c241b29f0268b0113a312538134087ca2b1893f
PeoVip <levanpeo1213@gmail.com>
2025-02-17 15:42:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "AjieDev - Lavalink [Non SSL]",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lava-v3.ajieblogs.eu.org",
-      port: 80,
+      name: "Koi Node V4",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port:  3000,
       secure: false
     }
   ]
