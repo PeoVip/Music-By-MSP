548ef241ff00d06b90bdaedaf67624d81ce04106
PeoVip <levanpeo1213@gmail.com>
2025-02-24 16:51:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 module.exports = {
   TOKEN: "",
   language: "vi",
-  ownerID: ["657589847510482944", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "PENHFGOLD_", 
-  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
